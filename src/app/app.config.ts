00e5e46c9c8219ed4b99486664278c7e796b9862
Arseniy Lavronov <a.lavronov@wis.software>
2026-03-30 01:23:59 +0300

    chore: configure PrimeNG and global styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,60 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    providePrimeNG({
+      unstyled: true,
+      translation: {
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        monthNames: [
+          'Январь',
+          'Февраль',
+          'Март',
+          'Апрель',
+          'Май',
+          'Июнь',
+          'Июль',
+          'Август',
+          'Сентябрь',
+          'Октябрь',
+          'Ноябрь',
+          'Декабрь'
+        ],
+        monthNamesShort: [
+          'Январь',
+          'Февраль',
+          'Март',
+          'Апрель',
+          'Май',
+          'Июнь',
+          'Июль',
+          'Август',
+          'Сентябрь',
+          'Октябрь',
+          'Ноябрь',
+          'Декабрь'
+        ],
+        today: 'Сегодня',
+        clear: 'Очистить',
+        weekHeader: 'Нед',
+        dateFormat: 'dd.mm.yy',
+        firstDayOfWeek: 1,
+        chooseDate: 'Выберите дату',
+        prevMonth: 'Предыдущий месяц',
+        nextMonth: 'Следующий месяц',
+        prevYear: 'Предыдущий год',
+        nextYear: 'Следующий год'
+      }
+    })
   ]
 };
