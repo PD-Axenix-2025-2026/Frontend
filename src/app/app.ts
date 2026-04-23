a2f1254987e9bf8ea7d5a9a952dd9af15b917fce
Arseniy Lavronov <a.lavronov@wis.software>
2026-04-23 20:27:09 +0300

    core: add local fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, computed, inject } from '@angular/core';
-import {Header} from './shared/layout/header/header';
-import {Hero} from './shared/ui/hero/hero';
-import {SearchForm} from './feature/search-form/search-form';
-import {SearchStats} from './feature/search-stats/search-stats';
-import {RouteFilters} from './feature/route-filters/route-filters';
-import {ResultsToolbar} from './feature/route-results/results-toolbar/results-toolbar';
-import {RouteCard, RouteCardItem} from './feature/route-results/route-card/route-card';
+import { Header } from './shared/layout/header/header';
+import { Hero } from './shared/ui/hero/hero';
+import { SearchForm } from './feature/search-form/search-form';
+import { SearchStats } from './feature/search-stats/search-stats';
+import { RouteFilters } from './feature/route-filters/route-filters';
+import { ResultsToolbar } from './feature/route-results/results-toolbar/results-toolbar';
+import { RouteCard } from './feature/route-results/route-card/route-card';
 import { SearchCreateRequest } from './core/api';
 import { SearchStateService } from './feature/search/search-state.service';
 import { mapRouteToCard } from './feature/route-results/route-card/route-card.mapper';
@@ -19,16 +19,15 @@ import { mapRouteToCard } from './feature/route-results/route-card/route-card.ma
 export class App {
   protected readonly searchState = inject(SearchStateService);
 
-  protected readonly routeCards = computed(() =>
-    this.searchState.items().map(mapRouteToCard)
-  );
+  protected readonly routeCards = computed(() => this.searchState.items().map(mapRouteToCard));
 
   protected readonly hasResults = computed(() => this.routeCards().length > 0);
-  protected readonly hasSearchStarted = computed(() =>
-    this.searchState.searchId() !== null ||
-    this.searchState.isLoading() ||
-    this.searchState.isPolling() ||
-    this.searchState.results() !== null
+  protected readonly hasSearchStarted = computed(
+    () =>
+      this.searchState.searchId() !== null ||
+      this.searchState.isLoading() ||
+      this.searchState.isPolling() ||
+      this.searchState.results() !== null,
   );
 
   onSearchSubmitted(payload: SearchCreateRequest) {
